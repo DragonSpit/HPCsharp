feef5bd00456b921cca5ddaa4f99810e6706477c
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2018-02-25 08:46:03 -0500

    Added and debugged performance measurements for Array and List sequence equals. Shows performance benefits as soon as the new user downloads the package.

---
+++ HPCsharpExamples/HPCsharpExamples/MeasureSequenceEqualPerformance.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using FooState;
+
+namespace HPCsharpExamples
+{
+    partial class Program
+    {
+        public static void MeasureArraySpeedup()
+        {
+            Random randNum = new Random(2);
+            int arraySize = 16 * 1024 * 1024;
+            int[] benchArrayOne = new int[arraySize];
+            int[] benchArrayTwo = new int[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                benchArrayOne[i] = randNum.Next(Int32.MinValue, Int32.MaxValue);    // fill array with random value between min and max
+                benchArrayTwo[i] = benchArrayOne[i];
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            long frequency = Stopwatch.Frequency;
+            long nanosecPerTick = (1000L * 1000L * 1000L) / frequency;
+
+            stopwatch.Restart();
+            bool equalParallel = benchArrayOne.FsParSequenceEqual(benchArrayTwo);
+            stopwatch.Stop();
+            double timeForParallelEqual = stopwatch.ElapsedTicks * nanosecPerTick / 1000000000.0;
+            stopwatch.Restart();
+            bool equal = benchArrayOne.SequenceEqual(benchArrayTwo);
+            stopwatch.Stop();
+            double timeForCsharpEqual = stopwatch.ElapsedTicks * nanosecPerTick / 1000000000.0;
+            Console.WriteLine("C# array equal {0}, Parallel equal {1} speedup {2:0.00}",
+                               timeForCsharpEqual, timeForParallelEqual, timeForCsharpEqual / timeForParallelEqual);
+
+            // Measure parallel array equality speedup
+            stopwatch.Restart();
+            bool equalSequencial = benchArrayOne.FsSeqSequenceEqual(benchArrayTwo);
+            stopwatch.Stop();
+            double timeForSequentialEqual = stopwatch.ElapsedTicks * nanosecPerTick / 1000000000.0;
+            Console.WriteLine("C# array equal {0}, Sequential equal {1} speedup {2:0.00}",
+                               timeForCsharpEqual, timeForSequentialEqual, timeForCsharpEqual / timeForSequentialEqual);
+        }
+
+        public static void MeasureListSpeedup()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Random randNum = new Random(2);
+            int ListSize = 16 * 1024 * 1024;
+            List<int> benchListOne = new List<int>(ListSize);
+            List<int> benchListTwo = new List<int>(ListSize);
+
+            for (int i = 0; i < ListSize; i++)
+            {
+                benchListOne.Add(randNum.Next(Int32.MinValue, Int32.MaxValue));    // fill lists with random value between min and max
+                benchListTwo.Add(benchListOne[i]);
+            }
+
+            long frequency = Stopwatch.Frequency;
+            long nanosecPerTick = (1000L * 1000L * 1000L) / frequency;
+
+            stopwatch.Restart();
+            bool equalParallel = benchListOne.FsParSequenceEqual(benchListTwo);
+            stopwatch.Stop();
+            double timeForParallelEqual = stopwatch.ElapsedTicks * nanosecPerTick / 1000000000.0;
+            stopwatch.Restart();
+            bool equal = benchListOne.SequenceEqual(benchListTwo);
+            stopwatch.Stop();
+            double timeForCsharpEqual = stopwatch.ElapsedTicks * nanosecPerTick / 1000000000.0;
+            Console.WriteLine("C# List equal {0}, Parallel equal {1} speedup {2:0.00}",
+                                timeForCsharpEqual, timeForParallelEqual, timeForCsharpEqual / timeForParallelEqual);
+
+            // Measure parallel array equality speedup
+            stopwatch.Restart();
+            bool equalSequencial = benchListOne.FsSeqSequenceEqual(benchListTwo);
+            stopwatch.Stop();
+            double timeForSequentialEqual = stopwatch.ElapsedTicks * nanosecPerTick / 1000000000.0;
+            Console.WriteLine("C# List equal {0}, Sequential equal {1} speedup {2:0.00}",
+                                timeForCsharpEqual, timeForSequentialEqual, timeForCsharpEqual / timeForSequentialEqual);
+        }
+    }
+}
--- HPCsharpExamples/HPCsharpExamples/Program.cs
+++ HPCsharpExamples/HPCsharpExamples/Program.cs
@@ -1,17 +1,15 @@
-﻿using System;
+﻿using FooState;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FooState;
 
 namespace HPCsharpExamples
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            // Check equality
+            // Check array equality
             int[] arrayOne = { 21, 43, 16, 5, 4, 0 };
             int[] arrayTwo = { 21, 43, 16, 5, 4, 0 };
 
@@ -19,16 +17,41 @@ namespace HPCsharpExamples
             bool equalSeq  = arrayOne.FsSeqSequenceEqual(arrayTwo);    // FooState non-parallel/sequential SequenceEqual
             bool equalPar  = arrayOne.FsParSequenceEqual(arrayTwo);    // FooState parallel                SequenceEqual
 
-            Console.WriteLine("Check equality: {0} {1} {2}", equalLinq, equalSeq, equalPar);
+            Console.WriteLine("Check array equality: {0} {1} {2}", equalLinq, equalSeq, equalPar);
 
-            // Check inequality
+            // Check array inequality
             int[] arrayThree = { 21, 43, 16, 3, 4, 0 };     // one element is different
 
             equalLinq = arrayOne.SequenceEqual(     arrayThree);    // System.Linq.SequenceEqual()
             equalSeq  = arrayOne.FsSeqSequenceEqual(arrayThree);    // FooState non-parallel/sequential SequenceEqual
             equalPar  = arrayOne.FsParSequenceEqual(arrayThree);    // FooState parallel                SequenceEqual
 
-            Console.WriteLine("Check inequality: {0} {1} {2}", equalLinq, equalSeq, equalPar);
+            Console.WriteLine("Check array inequality: {0} {1} {2}", equalLinq, equalSeq, equalPar);
+
+            // Check List equality
+            List<int> listOne = new List<int>() { 21, 43, 16, 5, 4, 0 };
+            List<int> listTwo = new List<int>() { 21, 43, 16, 5, 4, 0 };
+
+            equalLinq = listOne.SequenceEqual(     listTwo);    // System.Linq.SequenceEqual()
+            equalSeq  = listOne.FsSeqSequenceEqual(listTwo);    // FooState non-parallel/sequential SequenceEqual
+            equalPar  = listOne.FsParSequenceEqual(listTwo);    // FooState parallel                SequenceEqual
+
+            Console.WriteLine("Check list equality: {0} {1} {2}", equalLinq, equalSeq, equalPar);
+
+            // Check List inequality
+            List<int> listThree = new List<int>() { 21, 43, 16, 3, 4, 0 };     // one element is different
+
+            equalLinq = listOne.SequenceEqual(     listThree);    // System.Linq.SequenceEqual()
+            equalSeq  = listOne.FsSeqSequenceEqual(listThree);    // FooState non-parallel/sequential SequenceEqual
+            equalPar  = listOne.FsParSequenceEqual(listThree);    // FooState parallel                SequenceEqual
+
+            Console.WriteLine("Check list inequality: {0} {1} {2}", equalLinq, equalSeq, equalPar);
+
+            // Measure array equality speedup (parallel and sequential)
+            MeasureArraySpeedup();
+
+            // Measure List equality speedup (parallel and sequential)
+            MeasureListSpeedup();
         }
     }
 }
