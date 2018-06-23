e6e339446e522bfa39b0d695bcc7703eccaed0c3
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2018-06-23 16:32:13 -0400

    Parallel merge and parallel copy algorithms. Parallel Merge is ported from my C++ implementation.

---
+++ HPCsharp/CopyParallel.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace HPCsharper
+{
+    static public partial class ParallelAlgorithm
+    {
+        // Pays off on quad memory channel CPU, where one core may not be able to use all memory bandwidth with a single core
+        // Not only do Xeon's have quad-channel memory, but also some high end i7's
+        /// <summary>
+        /// Array smaller than this value will not be copied using a parallel copy
+        /// </summary>
+        public static Int32 CopyParArrayThreshold { get; set; } = 8 * 1024;
+        /// <summary>
+        /// List smaller than this value will not be copied using a parallel copy
+        /// </summary>
+        public static Int32 CopyParListThreshold { get; set; } = 16 * 1024;
+        /// <summary>
+        /// Amount of parallelism used for array copy, since most of the time it's not necessary to use all cores to use all of memory bandwidth
+        /// </summary>
+        public static Int32 CopyParParallelism { get; set; } = 2;
+        /// <summary>
+        /// Copy elements from the source array to the destination array
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source array</param>
+        /// <param name="srcStart">source array starting index</param>
+        /// <param name="dst">destination array</param>
+        /// <param name="dstStart">destination array starting index</param>
+        /// <param name="length">number of array elements to copy</param>
+        public static void CopyPar<T>(T[] src, Int32 srcStart, T[] dst, Int32 dstStart, Int32 length)
+        {
+            if (length <= 0)      // zero elements to copy
+                return;
+            if (length < CopyParArrayThreshold)
+            {
+                Array.Copy(src, srcStart, dst, dstStart, length);
+                return;
+            }
+
+            int lengthFirstHalf = length / 2;
+            int lengthSecondHalf = length - lengthFirstHalf;
+            Parallel.Invoke(
+                () => { CopyPar<T>(src, srcStart,                   dst, dstStart,                   lengthFirstHalf ); },
+                () => { CopyPar<T>(src, srcStart + lengthFirstHalf, dst, dstStart + lengthFirstHalf, lengthSecondHalf); }
+            );
+            return;
+        }
+        /// <summary>
+        /// Copy elements from the source array to the destination array
+        /// </summary>
+        /// <typeparam name="T">data type of each array element</typeparam>
+        /// <param name="src">source array</param>
+        /// <param name="dst">destination array</param>
+        /// <param name="length">number of array elements to copy</param>
+        public static void CopyPar<T>(this T[] src, T[] dst, Int32 length)
+        {
+            CopyPar<T>(src, 0, dst, 0, length);
+        }
+        /// <summary>
+        /// Copy elements from the source List to the destination List
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="srcStart">source List starting index</param>
+        /// <param name="dst">destination List</param>
+        /// <param name="dstStart">destination List starting index</param>
+        /// <param name="length">number of List elements to copy</param>
+// TODO: Detect and use C# well implemented cases for List, and use our code for the cases where C# does not implement well
+        private static void CopyParallelInner<T>(List<T> src, Int32 srcStart, List<T> dst, Int32 dstStart, Int32 length)
+        {
+            if (length <= 0)      // zero elements to copy
+                return;
+            if (length < CopyParListThreshold)
+            {
+                for (Int32 i = 0; i < length; i++)
+                    dst[dstStart++] = src[srcStart++];
+                return;
+            }
+
+            int lengthFirstHalf = length / 2;
+            int lengthSecondHalf = length - lengthFirstHalf;
+            Parallel.Invoke(
+                () => { CopyParallelInner<T>(src, srcStart,                   dst, dstStart,                   lengthFirstHalf ); },
+                () => { CopyParallelInner<T>(src, srcStart + lengthFirstHalf, dst, dstStart + lengthFirstHalf, lengthSecondHalf); }
+            );
+            return;
+        }
+#if false
+        // TODO: Keep in mind that List is different than Array and can expand in size. We may want to allow for this in our implementation to make it
+        //       more thoughtful and more general and more useful
+        // Idea: Create two versions: one that does not grow the destination List and one that does, possibly with one where its an error
+        //       to not have enough elements in the source or needing to clip the source. Examine each possible case one by one and figure out
+        //       the appropriate action for List. Then if we need to create separate functions to handle them, then just do it.
+        public static void CopyPar<T>(List<T> src, Int32 srcStart, List<T> dst, Int32 dstStart, Int32 length)
+        {
+            if (srcStart == 0 && dstStart == 0)
+            {
+                if (src.Count == dst.Count && src.Count == length)
+                {
+                    dst.Clear();
+                    dst.AddRange(src);
+                }
+                else if ()
+                {
+
+                }
+                
+                // use List.Delete and List.Insert
+                // or if dst List is empty use .AddRange
+            }
+            else
+            {
+
+            }
+        }
+#endif
+        /// <summary>
+        /// List smaller than this value will not be copied using a parallel copy
+        /// </summary>
+        public static Int32 CopyParallelListToArrayThreshold { get; set; } = 16 * 1024;    // TODO: don't guess, find threshold
+        /// <summary>
+        /// Amount of parallelism used for array copy, since most of the time it's not necessary to use all cores to use all of memory bandwidth
+        /// </summary>
+        public static Int32 CopyToParParallelism { get; set; } = 2;
+        private static void CopyParallelInner<T>(this List<T> src, Int32 srcStart, T[] dst, Int32 dstStart, Int32 length)
+        {
+            if (length <= 0)      // zero elements to copy
+                return;
+            if (length < CopyParallelListToArrayThreshold)
+            {
+                src.CopyTo(srcStart, dst, dstStart, length);
+                return;
+            }
+            int lengthFirstHalf  = length / 2;
+            int lengthSecondHalf = length - lengthFirstHalf;
+            var options = new ParallelOptions { MaxDegreeOfParallelism = CopyToParParallelism };
+            Parallel.Invoke(options,
+                () => { CopyParallelInner<T>(src, srcStart,                   dst, dstStart,                   lengthFirstHalf ); },
+                () => { CopyParallelInner<T>(src, srcStart + lengthFirstHalf, dst, dstStart + lengthFirstHalf, lengthSecondHalf); }
+            );
+            return;
+        }
+        /// <summary>
+        /// Copy elements from the source List to the destination Array
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="srcStart">source List starting index</param>
+        /// <param name="dst">destination Array</param>
+        /// <param name="dstStart">destination Array starting index</param>
+        /// <param name="length">number of Array elements to copy</param>
+        public static void CopyToPar<T>(this List<T> src, Int32 srcStart, T[] dst, Int32 dstStart, Int32 length)
+        {
+            CopyParallelInner<T>(src, srcStart, dst, dstStart, length);
+        }
+        /// <summary>
+        /// Copy elements from the source List to the destination Array
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="dst">destination Array</param>
+        /// <param name="length">number of elements to copy</param>
+        public static void CopyToPar<T>(this List<T> src, T[] dst, Int32 length)
+        {
+            CopyParallelInner<T>(src, 0, dst, 0, length);
+        }
+        /// <summary>
+        /// Copy elements from the source List to the destination Array
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="dst">destination Array</param>
+        public static void CopyToPar<T>(this List<T> src, T[] dst)
+        {
+            CopyParallelInner<T>(src, 0, dst, 0, src.Count);
+        }
+        /// <summary>
+        /// Create a new Array from the source List
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source List</param>
+        public static T[] ToArrayPar<T>(this List<T> src)
+        {
+            T[] dst = new T[src.Count];
+            CopyParallelInner<T>(src, 0, dst, 0, src.Count);
+            return dst;
+        }
+        /// <summary>
+        /// Create a new Array from a portion of source List
+        /// </summary>
+        /// <typeparam name="T">data type of each element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="srcStart">starting index within src List</param>
+        /// <param name="length">number of elements to be copied</param>
+        public static T[] ToArrayPar<T>(this List<T> src, Int32 srcStart, Int32 length)
+        {
+            T[] dst = new T[length];
+            CopyParallelInner<T>(src, srcStart, dst, 0, length);
+            return dst;
+        }
+    }
+}
---
+++ HPCsharp/MergeParallel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HPCsharper
+{
+    /// <summary>
+    /// Parallel Algorithms operating on variety of containers, providing trade-off between abstraction and performance
+    /// </summary>
+    static public partial class ParallelAlgorithm
+    {
+        private static void Exchange<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        /// <summary>
+        /// Smaller than threshold will use non-parallel algorithm to merge arrays
+        /// </summary>
+        static Int32 MergeParallelArrayThreshold { get; set; } = 64000;
+        /// <summary>
+        /// Divide-and-Conquer Merge of two ranges of source array src[ p1 .. r1 ] and src[ p2 .. r2 ] into destination array starting at index p3.
+        /// </summary>
+        /// <typeparam name="T">data type of each array element</typeparam>
+        /// <param name="src">source array</param>
+        /// <param name="p1">starting index of the first  segment, inclusive</param>
+        /// <param name="r1">ending   index of the first  segment, inclusive</param>
+        /// <param name="p2">starting index of the second segment, inclusive</param>
+        /// <param name="r2">ending   index of the second segment, inclusive</param>
+        /// <param name="dst">destination array</param>
+        /// <param name="p3">starting index of the result</param>
+        /// <param name="comparer">method to compare array elements</param>
+        public static void MergePar<T>(T[] src, Int32 p1, Int32 r1, Int32 p2, Int32 r2, T[] dst, Int32 p3, Comparer<T> comparer = null)
+        {
+            Int32 length1 = r1 - p1 + 1;
+            Int32 length2 = r2 - p2 + 1;
+            if (length1 < length2)
+            {
+                Exchange(ref p1, ref p2);
+                Exchange(ref r1, ref r2);
+                Exchange(ref length1, ref length2);
+            }
+            if (length1 == 0) return;
+            if ((length1 + length2) <= MergeParallelArrayThreshold)
+            {   // 8192 threshold is much better than 16 (is it for C#)
+                HPCsharp.Algorithm.Merge<T>(src, p1, p1 + length1, src, p2, p2 + length2, dst, p3, comparer);  // in DDJ paper
+            }
+            else
+            {
+                Int32 q1 = (p1 + r1) / 2;
+                Int32 q2 = Algorithm.BinarySearch(src[q1], src, p2, r2, comparer);
+                Int32 q3 = p3 + (q1 - p1) + (q2 - p2);
+                dst[q3] = src[q1];
+                Parallel.Invoke(
+                    () => { MergePar<T>(src, p1, q1 - 1, p2, q2 - 1, dst, p3, comparer); },
+                    () => { MergePar<T>(src, q1 + 1, r1, q2, r2, dst, q3 + 1, comparer); }
+                );
+            }
+        }
+        /// <summary>
+        /// Smaller than threshold will use non-parallel algorithm to merge arrays
+        /// </summary>
+        static Int32 MergeParallelListThreshold { get; set; } = 64000;
+#if false
+        // TODO: Figure out why this is so slow and does not accelerate
+        /// <summary>
+        /// Divide-and-Conquer Merge of two ranges of source List src[ p1 .. r1 ] and src[ p2 .. r2 ] into destination List starting at index p3.
+        /// </summary>
+        /// <typeparam name="T">data type of each List element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="p1">starting index of the first  segment, inclusive</param>
+        /// <param name="r1">ending   index of the first  segment, inclusive</param>
+        /// <param name="p2">starting index of the second segment, inclusive</param>
+        /// <param name="r2">ending   index of the second segment, inclusive</param>
+        /// <param name="dst">destination List</param>
+        /// <param name="p3">starting index of the result</param>
+        /// <param name="comparer">method to compare array elements</param>
+        public static void MergeParallel<T>(List<T> src, Int32 p1, Int32 r1, Int32 p2, Int32 r2, List<T> dst, Int32 p3, Comparer<T> comparer = null)
+        {
+            Int32 length1 = r1 - p1 + 1;
+            Int32 length2 = r2 - p2 + 1;
+            if (length1 < length2)
+            {
+                Exchange(ref p1, ref p2);
+                Exchange(ref r1, ref r2);
+                Exchange(ref length1, ref length2);
+            }
+            if (length1 == 0) return;
+            if ((length1 + length2) <= MergeParallelListThreshold)
+            {   // 8192 threshold is much better than 16 (is it for C#)
+                HPCsharp.Algorithm.Merge<T>(src, p1, p1 + length1, src, p2, p2 + length2, dst, p3, comparer);  // in DDJ paper
+            }
+            else
+            {
+                Int32 q1 = (p1 + r1) / 2;
+                Int32 q2 = Algorithm.BinarySearch(src[q1], src, p2, r2);
+                Int32 q3 = p3 + (q1 - p1) + (q2 - p2);
+                dst[q3] = src[q1];
+                Parallel.Invoke(
+                    () => { MergeParallel<T>(src, p1,     q1 - 1, p2, q2 - 1, dst, p3,     comparer); },
+                    () => { MergeParallel<T>(src, q1 + 1, r1,     q2, r2,     dst, q3 + 1, comparer); }
+                );
+            }
+        }
+#endif
+        /// <summary>
+        /// Divide-and-Conquer Merge of two ranges of source List src[ p1 .. r1 ] and src[ p2 .. r2 ] into destination List starting at index p3.
+        /// </summary>
+        /// <typeparam name="T">data type of each List element</typeparam>
+        /// <param name="src">source List</param>
+        /// <param name="p1">starting index of the first  segment, inclusive</param>
+        /// <param name="r1">ending   index of the first  segment, inclusive</param>
+        /// <param name="p2">starting index of the second segment, inclusive</param>
+        /// <param name="r2">ending   index of the second segment, inclusive</param>
+        /// <param name="dstStart">starting index of the result</param>
+        /// <param name="comparer">method to compare array elements</param>
+        public static List<T> MergePar<T>(List<T> src, Int32 p1, Int32 r1, Int32 p2, Int32 r2, Int32 dstStart, Comparer<T> comparer = null)
+        {
+            T[] srcCopy = src.ToArrayPar();
+            T[] dstCopy = new T[src.Count];
+            MergePar(srcCopy, p1, r1, p2, r2, dstCopy, dstStart, comparer);
+            return new List<T>(dstCopy);
+        }
+    }
+}
