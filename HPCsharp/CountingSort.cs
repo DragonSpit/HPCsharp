0464727b12c89c4d81efb5ecb470968af9b3292b
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-01-18 23:17:31 -0500

    Experimenting with variety of implementations for Fill of Array. Discovered that .NET core has an array.Fill method, but probably not a parallel version

--- HPCsharp/CountingSort.cs
+++ HPCsharp/CountingSort.cs
@@ -2,9 +2,15 @@
 //       a single core. Hope this works for ushort too.
 // TODO: Can we allocate the count array on the stack to make sorting small arrays faster?
 // TODO: Benchmark Fill() for different data types from byte (8-bits) to ulong (64-bits) to see if all of memory bandwidth can be used up. If not then go parallel.
+// TODO: If Fill() benchmarks well for larger data types, then reading the array 64-bits at a time will most likely pay off as well. Yes it does! Great direction to go in.
+// TODO: Consider using SIMD instructions to read and write even more bits per iteration - e.g. 256-bits is 32 bytes and 512-bits is an entire cache line.
+//       https://stackoverflow.com/questions/31999479/using-simd-operation-from-c-sharp-in-net-framework-4-6-is-slower
+
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
 
 namespace HPCsharp
 {
@@ -78,6 +84,16 @@ namespace HPCsharp
                 arrayToFill[i] = value;
         }
 
+        public static void FillPar<T>(this T[] arrayToFill, T value)
+        {
+            int m = arrayToFill.Length / 2;
+            int lengthOf2ndHalf = arrayToFill.Length - m;
+            Parallel.Invoke(
+                () => { Fill<T>(arrayToFill, 0, m,               value); },
+                () => { Fill<T>(arrayToFill, m, lengthOf2ndHalf, value); }
+            );
+        }
+
         public static void Fill<T>(this T[] arrayToFill, int startIndex, int length, T value)
         {
             int index    = startIndex;
@@ -86,6 +102,16 @@ namespace HPCsharp
                 arrayToFill[i] = value;
         }
 
+        public static void FillSse(this byte[] arrayToFill, byte value)
+        {
+            var fillVector = new Vector<byte>(value);
+            int numFullVectorsIndex = (arrayToFill.Length / Vector<byte>.Count) * Vector<byte>.Count;
+            int i;
+            for(i = 0; i < numFullVectorsIndex; i += Vector<byte>.Count)
+                fillVector.CopyTo(arrayToFill, i);
+            for (; i < arrayToFill.Length; i++)
+                arrayToFill[i] = value;
+        }
         // From StackOverflow fast fill question https://stackoverflow.com/questions/1897555/what-is-the-equivalent-of-memset-in-c
         public static void MemSetBlockX2(int[] array, int value)
         {
@@ -100,6 +126,23 @@ namespace HPCsharp
             {
                 int actualBlockSize = Math.Min(block, length - index);
                 Buffer.BlockCopy(array, 0, array, index << 2, actualBlockSize << 2);
+                index += block;
+                block *= 2;
+            }
+        }
+        public static void MemSetBlockX2(byte[] array, byte value)
+        {
+            int block = 32, index = 0;
+            int length = Math.Min(block, array.Length);
+
+            while (index < length)          // Fill the initial array
+                array[index++] = value;
+
+            length = array.Length;
+            while (index < length)
+            {
+                int actualBlockSize = Math.Min(block, length - index);
+                Buffer.BlockCopy(array, 0, array, index, actualBlockSize);
                 index += block;
                 block *= 2;
             }
