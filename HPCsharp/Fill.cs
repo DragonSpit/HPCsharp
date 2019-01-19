e5b09f1c13087f08adae6e0bee867ec920740558
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-01-19 08:26:37 -0500

    Separate Fill into separate files. Renamed Count as Histogram and split it out.

---
+++ HPCsharp/Fill.cs
@@ -0,0 +1,70 @@
+﻿// TODO: Use some suggestions from https://stackoverflow.com/questions/1897555/what-is-the-equivalent-of-memset-in-c and also parallelize if bandwidth is not used up by
+//       a single core. Hope this works for ushort too.
+// TODO: Can we allocate the count array on the stack to make sorting small arrays faster?
+// TODO: Benchmark Fill() for different data types from byte (8-bits) to ulong (64-bits) to see if all of memory bandwidth can be used up. If not then go parallel.
+// TODO: If Fill() benchmarks well for larger data types, then reading the array 64-bits at a time will most likely pay off as well. Yes it does! Great direction to go in.
+// TODO: Consider using SIMD instructions to read and write even more bits per iteration - e.g. 256-bits is 32 bytes and 512-bits is an entire cache line.
+//       https://stackoverflow.com/questions/31999479/using-simd-operation-from-c-sharp-in-net-framework-4-6-is-slower
+// TODO: Make sure to mention that .NET core has a Fill method implemented already. Modify my version to have the same interface, and provide a parallel version that's even faster.
+// TODO: Benchmark my Fill version on a quad-memory channel system to see how much bandwidth it provides - the fill rate! ;-) kinda like graphics.
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace HPCsharp
+{
+    static public partial class Algorithm
+    {
+        public static void Fill<T>(this T[] arrayToFill, T value)
+        {
+            for (int i = 0; i < arrayToFill.Length; i++)
+                arrayToFill[i] = value;
+        }
+
+        public static void Fill<T>(this T[] arrayToFill, int startIndex, int length, T value)
+        {
+            int index    = startIndex;
+            int endIndex = startIndex + length;
+            for (int i = startIndex; i < endIndex; i++)
+                arrayToFill[i] = value;
+        }
+
+        // From StackOverflow fast fill question https://stackoverflow.com/questions/1897555/what-is-the-equivalent-of-memset-in-c
+        public static void FillWithBlockCopy(int[] array, int value)
+        {
+            int block = 32, index = 0;
+            int length = Math.Min(block, array.Length);
+
+            while (index < length)          // Fill the initial array
+                array[index++] = value;
+
+            length = array.Length;
+            while (index < length)
+            {
+                int actualBlockSize = Math.Min(block, length - index);
+                Buffer.BlockCopy(array, 0, array, index << 2, actualBlockSize << 2);
+                index += block;
+                block *= 2;
+            }
+        }
+        public static void FillWithBlockCopy(byte[] array, byte value)
+        {
+            int block = 32, index = 0;
+            int length = Math.Min(block, array.Length);
+
+            while (index < length)          // Fill the initial array
+                array[index++] = value;
+
+            length = array.Length;
+            while (index < length)
+            {
+                int actualBlockSize = Math.Min(block, length - index);
+                Buffer.BlockCopy(array, 0, array, index, actualBlockSize);
+                index += block;
+                block *= 2;
+            }
+        }
+    }
+}
---
+++ HPCsharp/Histogram.cs
@@ -0,0 +1,48 @@
+﻿// TODO: Add histogramming of arrays of other data types, with byte and ushort counts
+// TODO: Add histogramming of 2-D and jagged arrays of variety of data types, with byte and ushort counts
+// TODO: Implement parallel histogram to support parallel Counting Sort
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace HPCsharp
+{
+    static public partial class Algorithm
+    {
+        public static int[] Histogram(this byte[] arrayToCount)
+        {
+            int numberOfBins = 256;
+            int[] counts = new int[numberOfBins];
+
+            for (uint currIndex = 0; currIndex < arrayToCount.Length; currIndex++)
+                counts[arrayToCount[currIndex]]++;
+
+            return counts;
+        }
+
+        public static int[] Histogram(this ushort[] arrayToCount)
+        {
+            int numberOfBins = 256 * 256;
+            int[] counts = new int[numberOfBins];
+
+            for (uint currIndex = 0; currIndex < arrayToCount.Length; currIndex++)
+                counts[arrayToCount[currIndex]]++;
+
+            return counts;
+        }
+
+        public static int[] Histogram(this short[] arrayToCount)
+        {
+            int numberOfBins = 256 * 256;
+            int[] counts = new int[numberOfBins];
+
+            // TODO: Since short values are signed and can be negative, we need to figure out a way to handle negative indexes
+            for (uint currIndex = 0; currIndex < arrayToCount.Length; currIndex++)
+                counts[arrayToCount[currIndex]]++;
+
+            return counts;
+        }
+    }
+}
