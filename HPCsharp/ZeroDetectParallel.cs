f510a1a606769e6775e2ae2a5f5009ab5b9d71c3
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-06-28 21:35:59 -0400

    Hid multi-core zero detect algorithms, since they are not scaling due to memory bandwidth limits. Implemented Value Detect, but not tested yet.

--- HPCsharp/ZeroDetectParallel.cs
+++ HPCsharp/ZeroDetectParallel.cs
@@ -1,7 +1,8 @@
 ﻿// TODO: See scalar ZeroDetection.cs for idea on constant value detection to implement.
-// TODO: Implement multi-core zero detection using the best SSE algorithm.
 // TODO: Improve implementation of equal of byte[] and other data types using the same technique and post to https://stackoverflow.com/questions/43289/comparing-two-byte-arrays-in-net?noredirect=1&lq=1
 //       once memory bandwidth limit has been reached by the implementation. This is the same type of a problem, where AND condition is needed.
+// TODO: I'm seeing that for .Sum() using float[] or double[] is faster than using integer[]. It could be that this pipeline is more optimal is some way than the integer pipeline. C# SIMD allows you to
+//       cast between data types. This may be worthwhile to try for additional performance.
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -82,6 +83,7 @@ namespace HPCsharp.ParallelAlgorithms
         private static bool ZeroDetectSseUnrolledInner(this byte[] arrayToOr, int l, int r)
         {
             var zeroVector = new Vector<byte>(0);
+            //var orVector   = new Vector<byte>(0);
             int concurrentAmount = 4;
             int sseIndexEnd = l + ((r - l + 1) / (Vector<byte>.Count * concurrentAmount)) * (Vector<byte>.Count * concurrentAmount);
             int i;
@@ -91,11 +93,11 @@ namespace HPCsharp.ParallelAlgorithms
             int increment = Vector<byte>.Count * concurrentAmount;
             for (i = l; i < sseIndexEnd; i += increment)
             {
-                var inVector  = new Vector<byte>(arrayToOr, i          );
-                inVector     |= new Vector<byte>(arrayToOr, i + offset1);
-                inVector     |= new Vector<byte>(arrayToOr, i + offset2);
-                inVector     |= new Vector<byte>(arrayToOr, i + offset3);
-                if (!Vector.EqualsAll(inVector, zeroVector))
+                var orVector = new Vector<byte>(arrayToOr, i);
+                orVector |= new Vector<byte>(arrayToOr, i + offset1);
+                orVector |= new Vector<byte>(arrayToOr, i + offset2);
+                orVector |= new Vector<byte>(arrayToOr, i + offset3);
+                if (!Vector.EqualsAll(orVector, zeroVector))
                     return false;
             }
             byte overallOr = 0;
@@ -109,11 +111,48 @@ namespace HPCsharp.ParallelAlgorithms
             return arrayToDetect.ZeroDetectSseUnrolledInner(0, arrayToDetect.Length - 1);
         }
 
-        public static bool ZeroDetectSseUnrolled(this byte[] arrayToProcess, int start, int length, int thresholdParallel = 4096)
+        public static bool ZeroDetectSseUnrolled(this byte[] arrayToProcess, int start, int length)
         {
             return arrayToProcess.ZeroDetectSseUnrolledInner(start, start + length - 1);
         }
 
+        private static bool ValueDetectSseUnrolledInner(this byte[] arrayToOr, int l, int r, byte value)
+        {
+            var valueVector = new Vector<byte>(value);
+            //var orVector   = new Vector<byte>(0);
+            int concurrentAmount = 4;
+            int sseIndexEnd = l + ((r - l + 1) / (Vector<byte>.Count * concurrentAmount)) * (Vector<byte>.Count * concurrentAmount);
+            int i;
+            int offset1 = Vector<byte>.Count;
+            int offset2 = Vector<byte>.Count * 2;
+            int offset3 = Vector<byte>.Count * 3;
+            int increment = Vector<byte>.Count * concurrentAmount;
+            for (i = l; i < sseIndexEnd; i += increment)
+            {
+                bool equals = Vector.EqualsAll(new Vector<byte>(arrayToOr, i), valueVector);
+                equals = equals && Vector.EqualsAll(new Vector<byte>(arrayToOr, i), valueVector);
+                equals = equals && Vector.EqualsAll(new Vector<byte>(arrayToOr, i), valueVector);
+                equals = equals && Vector.EqualsAll(new Vector<byte>(arrayToOr, i), valueVector);
+                if (!equals)
+                    return false;
+            }
+            bool overallEquals = true;
+            for (; i <= r; i++)
+                overallEquals = overallEquals && (arrayToOr[i] == value);
+            return overallEquals;
+        }
+
+        public static bool ValueDetectSseUnrolled(this byte[] arrayToDetect, byte value)
+        {
+            return arrayToDetect.ValueDetectSseUnrolledInner(0, arrayToDetect.Length - 1, value);
+        }
+
+        public static bool ValueDetectSseUnrolled(this byte[] arrayToProcess, int start, int length, byte value)
+        {
+            return arrayToProcess.ValueDetectSseUnrolledInner(start, start + length - 1, value);
+        }
+
+        // About 5% faster, due to no checking for non-zero on every loop iteration
         private static bool ZeroDetectSseUnrolledInner2(this byte[] arrayToOr, int l, int r)
         {
             var orVector0 = new Vector<byte>(0);
@@ -129,14 +168,10 @@ namespace HPCsharp.ParallelAlgorithms
             int increment = Vector<byte>.Count * concurrentAmount;
             for (i = l; i < sseIndexEnd; i += increment)
             {
-                var inVector0 = new Vector<byte>(arrayToOr, i);
-                var inVector1 = new Vector<byte>(arrayToOr, i + offset1);
-                var inVector2 = new Vector<byte>(arrayToOr, i + offset2);
-                var inVector3 = new Vector<byte>(arrayToOr, i + offset3);
-                orVector0 |= inVector0;
-                orVector1 |= inVector1;
-                orVector2 |= inVector2;
-                orVector3 |= inVector3;
+                orVector0 |= new Vector<byte>(arrayToOr, i);
+                orVector1 |= new Vector<byte>(arrayToOr, i + offset1);
+                orVector2 |= new Vector<byte>(arrayToOr, i + offset2);
+                orVector3 |= new Vector<byte>(arrayToOr, i + offset3);
             }
             byte overallOr = 0;
             for (; i <= r; i++)
@@ -154,7 +189,7 @@ namespace HPCsharp.ParallelAlgorithms
             return arrayToDetect.ZeroDetectSseUnrolledInner2(0, arrayToDetect.Length - 1);
         }
 
-        private static bool ZeroDetectSseUnrolledParInner(this byte[] arrayToProcess, int l, int r, int thresholdParallel = 4096)
+        private static bool ZeroDetectSseUnrolledParInner(this byte[] arrayToProcess, int l, int r, int thresholdParallel = 4096, int parallelism = 2)
         {
             bool partLeft = false;
 
@@ -167,7 +202,8 @@ namespace HPCsharp.ParallelAlgorithms
 
             bool partRight = false;
 
-            Parallel.Invoke(
+            var options = new ParallelOptions { MaxDegreeOfParallelism = parallelism };
+            Parallel.Invoke( options,
                 () => { partLeft  = ZeroDetectSseUnrolledParInner(arrayToProcess, l,     m); },
                 () => { partRight = ZeroDetectSseUnrolledParInner(arrayToProcess, m + 1, r); }
             );
@@ -175,12 +211,12 @@ namespace HPCsharp.ParallelAlgorithms
             return partLeft && partRight;
         }
 
-        public static bool ZeroDetectSseUnrolledPar(this byte[] arrayToProcess, int thresholdParallel = 4096)
+        public static bool ZeroDetectSseUnrolledPar(this byte[] arrayToProcess, int thresholdParallel = 4096, int parallelism = 2)
         {
             return ZeroDetectSseUnrolledParInner(arrayToProcess, 0, arrayToProcess.Length - 1, thresholdParallel);
         }
 
-        public static bool ZeroDetectSseUnrolledPar(this byte[] arrayToProcess, int start, int length, int thresholdParallel = 4096)
+        public static bool ZeroDetectSseUnrolledPar(this byte[] arrayToProcess, int start, int length, int thresholdParallel = 4096, int parallelism = 2)
         {
             return arrayToProcess.ZeroDetectSseUnrolledParInner(start, start + length - 1, thresholdParallel);
         }
