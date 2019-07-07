3dfd9b0ad9ac36f673cb17443abc95ef0c60a640
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-07-07 16:35:22 -0400

    Added unit tests for sum overflow exceptions

---
+++ HPCSharp.UnitTests/SumTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HPCsharp.Algorithms;
+using HPCsharp.ParallelAlgorithms;
+using NUnit.Framework;
+
+namespace HPCSharp.UnitTests
+{
+    [TestFixture]
+    public sealed class SumTests
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void ShouldThrowOverflowExceptionOrNotLong(int whichTestCase)
+        {
+            long[] arrLong  = new long[] { 5, 7, 16, 3, Int64.MaxValue, 1 };
+            long[] arrLong1 = new long[] { 5, 7, 16, Int64.MaxValue, 3, 1 };
+
+            if (whichTestCase == 0)
+            {
+                Assert.Throws<OverflowException>(() => arrLong.SumHpc());
+            }
+            else if (whichTestCase == 1)
+            {
+                Assert.Throws<OverflowException>(() => arrLong.SumSse());
+            }
+            else if (whichTestCase == 2)
+            {
+                Assert.DoesNotThrow(() => arrLong1.SumSse());
+            }
+        }
+    }
+}
