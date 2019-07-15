a22f810c665f614babc92c4bd80207acf15d847f
LAPTOP-465HP6VI\Victor <vduvanko1@gmail.com>
2019-07-14 22:33:48 -0400

    Implemented unrolled AddTo, which didn't help. Implemented multi-core divide-and-conquer AddTo, but it's broken and never returns (needs debugged)

--- HPCsharp/AddParallel.cs
+++ HPCsharp/AddParallel.cs
@@ -60,5 +60,73 @@ namespace HPCsharp.ParallelAlgorithms
             for (; i <= r; i++)
                 arrayA[i] += arrayB[i];
         }
+
+        private static void AddToSseUnrolled(this int[] arrayA, int[] arrayB)
+        {
+            arrayA.AddToSseUnrolledInner(arrayB, 0, arrayA.Length - 1);
+        }
+
+        private static void AddToSseUnrolled(this int[] arrayA, int[] arrayB, int start, int length)
+        {
+            arrayA.AddToSseUnrolledInner(arrayB, start, start + length - 1);
+        }
+        private static void AddToSseUnrolledInner(this int[] arrayA, int[] arrayB, int l, int r)
+        {
+            int concurrentAmount = 2;
+            int sseIndexEnd = l + ((r - l + 1) / (Vector<int>.Count * concurrentAmount)) * (Vector<int>.Count * concurrentAmount);
+            int offset1   = Vector<int>.Count;
+            int increment = Vector<int>.Count * concurrentAmount;
+            int i, j;
+            for (i = l, j = l + offset1; i < sseIndexEnd; i += increment, j += increment)
+            {
+                var inVectorA = new Vector<int>(arrayA, i);
+                var inVectorB = new Vector<int>(arrayB, i);
+                var inVectorC = new Vector<int>(arrayA, j);
+                var inVectorD = new Vector<int>(arrayB, j);
+                inVectorA += inVectorB;
+                inVectorC += inVectorD;
+                inVectorA.CopyTo(arrayA, i);
+                inVectorC.CopyTo(arrayA, j);
+            }
+            for (; i <= r; i++)
+                arrayA[i] += arrayB[i];
+        }
+
+        private static void AddToSseParInner(this int[] arrayA, int[] arrayB, int l, int r, int thresholdParallel = 16 * 1024)
+        {
+            if (l > r)
+                return;
+            if ((r - l + 1) <= thresholdParallel)
+                arrayA.AddToSseInner(arrayB, l, r - l + 1);
+
+            int m = (r + l) / 2;
+
+            Parallel.Invoke(
+                () => { arrayA.AddToSseParInner(arrayB, l,     m, thresholdParallel); },
+                () => { arrayA.AddToSseParInner(arrayB, m + 1, r, thresholdParallel); }
+            );
+        }
+
+        /// <summary>
+        /// Add two int[] arrays together, using multiple cores, and using data parallel SIMD/SSE instructions for higher performance within each core.
+        /// </summary>
+        /// <param name="arrayA">Input array and the result array</param>
+        /// <param name="arrayB">Second input array</param>
+        private static void AddToSsePar(this int[] arrayA, int[] arrayB, int thresholdParallel = 16 * 1024)
+        {
+            arrayA.AddToSseParInner(arrayB, 0, arrayA.Length - 1, thresholdParallel);
+        }
+
+        /// <summary>
+        /// Add two int[] arrays together, using multiple cores, and using data parallel SIMD/SSE instructions for higher performance within each core.
+        /// </summary>
+        /// <param name="arrayA">Input array and the result array</param>
+        /// <param name="arrayB">Second input array</param>
+        /// <param name="startIndex">index of the starting element for the summation</param>
+        /// <param name="length">number of array elements to sum up</param>
+        private static void AddToSsePar(this int[] arrayA, int[] arrayB, int startIndex, int length, int thresholdParallel = 16 * 1024)
+        {
+            arrayA.AddToSseParInner(arrayB, startIndex, startIndex + length - 1, thresholdParallel);
+        }
     }
 }
