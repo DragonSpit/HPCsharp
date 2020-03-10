85003951cb3a2b00df120b51e7b4ac04c3810eb6
Victor Duvanenko <vduvanenko1@gmail.com>
2020-03-09 21:41:32 -0400

    Added standard deviation compute algorithm, since it can use .Sum, which HPCsharp has accelerated nicely

---
+++ HPCsharp/Statistics.cs
@@ -0,0 +1,45 @@
+﻿// TODO: Since standard deviation computes input array average, it may be nice to return that average, since it is a useful value. Or, it needs to take an optional nullable average value (maybe).
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCsharp.Algorithms
+{
+    static public partial class Statistics
+    {
+        /// <summary>
+        /// Standard deviation of an array of integers.
+        /// </summary>
+        /// <param name="values">An array to sum up</param>
+        /// <returns>standard deviation as a double</returns>
+        public static double StandardDeviation(this int[] values)
+        {
+            double avg = values.Average();
+            return Math.Sqrt(values.Average(v => Math.Pow(v - avg, 2)));
+        }
+        /// <summary>
+        /// Standard deviation of an array of integers.
+        /// </summary>
+        /// <param name="values">An array to sum up</param>
+        /// <returns>standard deviation as a double</returns>
+        public static double StandardDeviation(this long[] values)
+        {
+            double avg = values.Average();
+            return Math.Sqrt(values.Average(v => Math.Pow(v - avg, 2)));
+        }
+    }
+
+    static public partial class StatisticsExperimental
+    {
+        // Computes a similar value to Standard Deviation, but simpler/faster to compute, since no squaring per element and Square Root. Uses absolute value instead, which has a discontinuity and can't be integrated
+        // Which means that it could be computed entirely using integer computation. Plus, no squarint means the resulting function is linear and doesn't warp the input data
+        public static double StandardDeviation_NonStandardMethod(this int[] values)
+        {
+            double avg = values.Average();
+            return values.Average(v => Math.Abs(v - avg));
+        }
+    }
+}
---
+++ HPCsharp/StatisticsParallel.cs
@@ -0,0 +1,37 @@
+﻿// TODO: StandardDeviation method along with .Average could be parallelized. See how much faster our implementation than Linq. How much faster is Linq parallel version versus serial.
+// TODO: This function can be accelerated substantially, possibly using SIMD/SSE, since instead of Math.Pow we could use multiplication, after casting to double in SSE.
+//       The flow from variety of integer data types would be to cast to double, then square by multiplying, then to sum up and divide by the number of elements to compute the average.
+//       yielding a much faster standard deviation computation, which uses SIMD and multi-core. Use parallel .Sum that uses integer computation, which should be much faster.
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCsharp.ParallelAlgorithms
+{
+    static public partial class Statistics
+    {
+        /// <summary>
+        /// Standard deviation of an array of integers.
+        /// </summary>
+        /// <param name="values">An array to sum up</param>
+        /// <returns>standard deviation as a double</returns>
+        public static double StandardDeviationPar(this int[] values)
+        {
+            double avg = values.AsParallel().Average();
+            return Math.Sqrt(values.AsParallel().Average(v => Math.Pow(v - avg, 2)));
+        }
+        /// <summary>
+        /// Standard deviation of an array of integers.
+        /// </summary>
+        /// <param name="values">An array to sum up</param>
+        /// <returns>standard deviation as a double</returns>
+        public static double StandardDeviationPar(this long[] values)
+        {
+            double avg = values.AsParallel().Average();
+            return Math.Sqrt(values.AsParallel().Average(v => Math.Pow(v - avg, 2)));
+        }
+    }
+}
