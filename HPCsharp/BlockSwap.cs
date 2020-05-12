e0dcee73faeaeefbd0f54448625b033764c7e7b9
Victor Duvanenko <vduvanenko1@gmail.com>
2020-05-11 22:28:48 -0400

    Implemented slightly parallel BlockSwapReversal algorithm, which sped up by 50%. However, it needs tuning, as we are getting into a condition of overly parallelizing, leading to severe memory contention

--- HPCsharp/BlockSwap.cs
+++ HPCsharp/BlockSwap.cs
@@ -1,6 +1,8 @@
 ﻿// TODO: Parallelize block-swap algorithm to speedup in-place merge, possibly using SSE with instruction to reverse order within an SSE Vector
 //       Or, maybe SSE rotation/reverse order can be avoided, knowing that we'll be rotating it back in the following pass.
-// TODO: Inline manually Swap function into Reversal
+// TODO: Parallelize block-swap algorithms to see if there is a benefit, now that unit testing and benchmarking in C# is in place
+//       Try simple things first like scalar parallelism of reversal algorithms middle stage of reversal by running the two portions reversal
+//       in parallel to see if it speeds up at all.
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -37,9 +39,15 @@ namespace HPCsharp
         }
 
         // reverse/mirror a range from l to r, inclusively, in-place
-        public static void Reversal<T>(this T[] inArray, int l, int r)
+        public static void Reversal<T>(this T[] array, int l, int r)
         {
-            while (l < r) inArray.Swap(l++, r--);
+            for(; l < r; l++, r--)
+            {
+                // inArray.Swap(l++, r--);
+                T temp   = array[l];
+                array[l] = array[r];
+                array[r] = temp;
+            }
         }
 
         // Swaps two sequential subarrays ranges a[ l .. m ] and a[ m + 1 .. r ]
---
+++ HPCsharp/BlockSwapParallel.cs
@@ -0,0 +1,26 @@
+﻿// TODO: Parallelize block-swap algorithm to speedup in-place merge, possibly using SSE with instruction to reverse order within an SSE Vector
+//       Or, maybe SSE rotation/reverse order can be avoided, knowing that we'll be rotating it back in the following pass.
+// TODO: Parallelize block-swap algorithms to see if there is a benefit, now that unit testing and benchmarking in C# is in place
+//       Try simple things first like scalar parallelism of reversal algorithms middle stage of reversal by running the two portions reversal
+//       in parallel to see if it speeds up at all.
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HPCsharp.ParallelAlgorithms;
+
+namespace HPCsharp
+{
+    static public partial class ParallelAlgorithm
+    {
+        // Swaps two sequential subarrays ranges a[ l .. m ] and a[ m + 1 .. r ]
+        public static void BlockSwapReversalPar<T>(T[] array, int l, int m, int r)
+        {
+            Parallel.Invoke(
+                () => { array.Reversal(l,     m); },
+                () => { array.Reversal(m + 1, r); }
+            );
+            array.Reversal(l, r);
+        }
+    }
+}
