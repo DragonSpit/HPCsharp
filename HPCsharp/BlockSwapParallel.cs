febf8531d35a5a2abcbefdc4a760d6456c5d56cf
Victor Duvanenko <vduvanenko1@gmail.com>
2020-05-15 07:29:18 -0400

    Parallel BlockSwap is now working, parallizing to two CPU cores for now, to see if that level of parallelism increases performance of this bottleneck part of the in-place merge and in-place merge sort algorithms. Yes! It speeds up nearly doubles in performance!

--- HPCsharp/BlockSwapParallel.cs
+++ HPCsharp/BlockSwapParallel.cs
@@ -3,10 +3,12 @@
 // TODO: Parallelize block-swap algorithms to see if there is a benefit, now that unit testing and benchmarking in C# is in place
 //       Try simple things first like scalar parallelism of reversal algorithms middle stage of reversal by running the two portions reversal
 //       in parallel to see if it speeds up at all.
+// TODO: Write a blog on parallelizing BlockSwap Reversal algorithm
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 using HPCsharp.ParallelAlgorithms;
 
 namespace HPCsharp
@@ -14,13 +16,30 @@ namespace HPCsharp
     static public partial class ParallelAlgorithm
     {
         // Swaps two sequential subarrays ranges a[ l .. m ] and a[ m + 1 .. r ]
-        public static void BlockSwapReversalPar<T>(T[] array, int l, int m, int r)
+        public static void BlockSwapReversalPar<T>(T[] array, int l, int m, int r, int threshold = 16 * 1024)
         {
-            Parallel.Invoke(
-                () => { array.Reversal(l,     m); },
-                () => { array.Reversal(m + 1, r); }
-            );
-            array.Reversal(l, r);
+            int length = r - l + 1;
+            if (length < threshold)
+            {
+                array.Reversal(l,     m);
+                array.Reversal(m + 1, r);
+                array.Reversal(l,     r);
+            }
+            else
+            {
+                Parallel.Invoke(
+                    () => { array.Reversal(l,     m); },
+                    () => { array.Reversal(m + 1, r); }
+                );
+                //array.Reversal(l, r);     // serial version of the rest of the code
+                int firstLength = ((length + 2) / 4) * 2;  // how much first  core will swap. Guaranteed to be an even value
+                int firstSwapLength = firstLength  / 2;
+                Parallel.Invoke(
+                    () => { array.Swap(l, r - firstSwapLength + 1, firstSwapLength, true); },   // all but the last part need to be in Swap(startA, startB, length) form, since these are outer rings
+                                                                                                // to parallelize more, add more of the above parts (more outer onion rings/layers)
+                    () => { array.Reversal(l + firstSwapLength, r - firstSwapLength); }         // the last/innermost part needs to be in Reversal(l, r) form, since its the inner core of the onion
+                );
+            }
         }
     }
 }
