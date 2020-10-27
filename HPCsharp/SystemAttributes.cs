94c08bfd6c150e8ad40bbb5b783d33dd2c8c9a60
Victor Duvanenko <vduvanenko1@gmail.com>
2020-10-27 07:35:16 -0400

    Added new namespace for system attributes, suhc as the number of hyperthreads per core, which will provide a place for more attributes in the future

---
+++ HPCsharp/SystemAttributes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HPCsharp
+{
+    static public partial class SystemAttributes
+    {
+        public static Int32 HyperthreadingNumberOfWays { get; set; } = 2;
+    }
+}
