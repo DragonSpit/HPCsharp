26c2eeedcbec3055b5ffb4eddc0aabd91b11831c
Victor J. Duvanenko <duvanenko@gmail.com>
2025-10-19 21:50:57 -0400

    Improved documentation for Radix Selection. Removed unused argument. Added a TODO for later simplification and redundancy removal.

--- HPCsharp/SelectionRadix.cs
+++ HPCsharp/SelectionRadix.cs
@@ -11,14 +11,8 @@ namespace HPCsharp
 {
     public static partial class Algorithm
     {
-        private static void RadixSelectiontMsdUIntInner(uint[] a, int first, int length, int shiftRightAmount, int k, int threshold = 1024)
+        private static void RadixSelectiontMsdUIntInner(uint[] a, int first, int length, int shiftRightAmount, int k)
         {
-            // TODO: Replace with QuickSelect random version for < threshold
-            //if (length < threshold)
-            //{
-            //    baseCaseInPlaceSort(a, first, length);
-            //    return;
-            //}
             int last = first + length - 1;
             const uint bitMask = PowerOfTwoRadix - 1;
 
@@ -38,6 +32,7 @@ namespace HPCsharp
                 if (binLength == 0) continue; // skip empty bins
                 if (k >= startOfBin[kthBin] && k <= (startOfBin[kthBin + 1] - 1)) break;
             }
+            // TODO: Turn the two while loops into a function, since they do the same work
             int _current_ob = first, _current_ib = startOfBin[kthBin], found_ob; // _ob = outside of bin, _ib = inside of bin
             while (true) // process elements outside the bin that k is in, which are to the left of that bin
             {
@@ -73,23 +68,18 @@ namespace HPCsharp
                 // Only recurse into the bin that contains the k-th smallest element and if more than one element is in that bin
                 if ((startOfBin[kthBin + 1] - startOfBin[kthBin]) > 1)
                     RadixSelectiontMsdUIntInner(a, startOfBin[kthBin], startOfBin[kthBin + 1] - startOfBin[kthBin], shiftRightAmount, k);
-                else if ((startOfBin[kthBin + 1] - startOfBin[kthBin]) == 1)
-                {
-                    return; // Only one element in the bin that k is in, so it must be the k-th smallest element
-                }
-                else
-                {
-                    // No elements in the bin that k is in, which should never happen
-                    throw new Exception("RadixSelectiontMsdUIntInner: No elements in the bin that k is in, which should never happen");
-                }
+                else if ((startOfBin[kthBin + 1] - startOfBin[kthBin]) == 1) return; // Only one element in the bin that k is in, so it must be the k-th smallest element
+                else throw new Exception("RadixSelectiontMsdUIntInner: No elements in the bin that k is in, which should never happen");
             }
         }
         /// <summary>
         /// In-place Radix Selection (Most Significant Digit).
         /// </summary>
         /// <param name="arrayToBeSelected">array that is to be selected from in place</param>
-        /// <param name="threshold">for array size smaller than threshold Array.Sort will be used instead</param>
-        public static uint SelectRadixMsd(this uint[] arrayToBeSelected, Int32 start, Int32 length, Int32 k, Int32 threshold = 1024)
+        /// <param name="start">starting index of the subarray</param>
+        /// <param name="length">length of the subarray</param>
+        /// <param name="k">index of the desired element to be selected</param>
+        public static uint SelectRadixMsd(this uint[] arrayToBeSelected, Int32 start, Int32 length, Int32 k)
         {
             if (arrayToBeSelected == null)
                 throw new ArgumentNullException(nameof(arrayToBeSelected));
@@ -98,27 +88,24 @@ namespace HPCsharp
             if (k < start || k > (start + arrayToBeSelected.Length))
                 throw new ArgumentOutOfRangeException(nameof(k), "k must be between start and (start + length)");
             int shiftRightAmount = (sizeof(uint) * 8) - Log2ofPowerOfTwoRadix;
-            // Insertion Sort or Heap Sort could be passed in as another base case since they are both in-place
-            RadixSelectiontMsdUIntInner(arrayToBeSelected, start, length, shiftRightAmount, k, threshold);
+            RadixSelectiontMsdUIntInner(arrayToBeSelected, start, length, shiftRightAmount, k);
             return arrayToBeSelected[k];
         }
         /// <summary>
         /// In-place Radix Selection (Most Significant Digit), not a stable sort.
         /// </summary>
         /// <param name="arrayToBeSelected">array that is to be sorted in place</param>
-        /// <param name="threshold">for array size smaller than threshold Array.Sort will be used instead</param>
-        public static uint SelectRadixMsd(this uint[] arrayToBeSelected, Int32 k, Int32 threshold = 1024)
+        /// <param name="k">index of the desired element to be selected</param>
+        public static uint SelectRadixMsd(this uint[] arrayToBeSelected, Int32 k)
         {
             if (arrayToBeSelected == null)
                 throw new ArgumentNullException(nameof(arrayToBeSelected));
             if (arrayToBeSelected.Length <= 0)
-                throw new ArgumentOutOfRangeException(nameof(k), "l or r are invalid");
+                throw new ArgumentOutOfRangeException(nameof(arrayToBeSelected.Length), "array length is invalid");
             if (k < 0 || k > arrayToBeSelected.Length)
                 throw new ArgumentOutOfRangeException(nameof(k), "k must be between start and (start + length)");
             int shiftRightAmount = (sizeof(uint) * 8) - Log2ofPowerOfTwoRadix;
-            //Console.WriteLine("Radix Selection:  k = {0}   Array length = {1}", k, arrayToBeSelected.Length);
-            // Insertion Sort or Heap Sort could be passed in as another base case since they are both in-place
-            RadixSelectiontMsdUIntInner(arrayToBeSelected, 0, arrayToBeSelected.Length, shiftRightAmount, k, threshold);
+            RadixSelectiontMsdUIntInner(arrayToBeSelected, 0, arrayToBeSelected.Length, shiftRightAmount, k);
             return arrayToBeSelected[k];
         }
 
