79526372a8f20c239cb414e6037fa7ae46045d79
Victor J. Duvanenko <duvanenko@gmail.com>
2026-06-05 22:56:54 -0400

    Implementing suffix sum to have it as a common function to reuse

---
+++ HPCsharp/SuffixSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCsharp
+{
+    static public partial class Algorithm
+    {
+        public static void SuffixSum(int[] inOutArray, int startIndex, int length, int first = 0)
+        {
+            ArgumentNullException.ThrowIfNull(inOutArray);
+            int prev = inOutArray[startIndex];                          // prev = count[0]
+            inOutArray[startIndex] = first;                             // count[0] = 0
+            int endIndex = startIndex + length - 1;
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+                int current = inOutArray[i];
+                inOutArray[i] = inOutArray[i - 1] + prev;
+                prev = current;
+            }
+        }
+    }
+}
