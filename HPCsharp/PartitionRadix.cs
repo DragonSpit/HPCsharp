4d540047baa1623e7debef564924259157674b5f
Victor J. Duvanenko <duvanenko@gmail.com>
2026-06-07 22:42:08 -0400

    Added Partition using Radix by k[]-th array - has not been tested yet.

--- HPCsharp/PartitionRadix.cs
+++ HPCsharp/PartitionRadix.cs
@@ -1,4 +1,6 @@
 ﻿// TODO: Implement in a stable way by using SelectRadix as the starting point and then keep the elements which are < k-th bin on the left of k-th bin and move elements which are > k-th bin on the right of k-th bin.
+// TODO: Implement numpy.partition like functionality where the k-th element is an actual value of array[k] instead of an index. (Note: The current implementation works like C++'s nth_element API).
+// TODO: Implement a k[] array verstion of the partitioning, as the current SelectRadixMsdUIntInner method most likely already supports this, but it needs to be verified and tested.
 
 using System;
 using System.Collections.Generic;
@@ -11,7 +13,7 @@ namespace HPCsharp
     public static partial class Algorithm
     {
         /// <summary>
-        /// In-place Partition using Radix by the k-th elements in an array. Not a stable algorithm.
+        /// In-place Partition using Radix by the k-th element in an array. Not a stable algorithm.
         /// </summary>
         /// <param name="arrayToBePartitioned">array that is to be selected in place</param>
         /// <param name="start">starting index of the subarray</param>
@@ -32,7 +34,7 @@ namespace HPCsharp
             //RadixSortMsdUIntInner(arrayToBePartitioned, start, length, shiftRightAmount, (arr, startIndex, lengthOfArray) => InsertionSort(arrayToBePartitioned, start, length), threshold);
         }
         /// <summary>
-        /// In-place Partition using Radix by the k-th elements in an array. Not a stable algorithm.
+        /// In-place Partition using Radix by the k-th element in an array. Not a stable algorithm.
         /// </summary>
         /// <param name="arrayToBeParitioned">array that is to be selected from in place</param>
         /// <param name="k">Index to be used as the partitioning element. The value of the arrayToBePartitioned[k] will not be used for partitioning.</param>
@@ -47,5 +49,51 @@ namespace HPCsharp
             int shiftRightAmount = (sizeof(uint) * 8) - Log2ofPowerOfTwoRadix;
             SelectRadixMsdUIntInner(arrayToBeParitioned, 0, arrayToBeParitioned.Length, shiftRightAmount, kArray, 0, kArray.Length, Array.Sort, threshold);
         }
+        /// <summary>
+        /// In-place Partition using Radix by the k[]-th elements in an array. Not a stable algorithm.
+        /// </summary>
+        /// <param name="arrayToBePartitioned">array that is to be partitioned in place</param>
+        /// <param name="start">starting index of the subarray</param>
+        /// <param name="length">length of the subarray</param>
+        /// <param name="k">array of indexes of the desired elements to be partitioned by, in ascending order</param>
+        /// <param name="threshold">for array size smaller than threshold Array.Sort will be used instead of MSD Radix Sort like algorithm</param>
+        public static void PartitionRadix(this uint[] arrayToBePartitioned, Int32 start, Int32 length, Int32[] k, Int32 threshold = 1024)
+        {
+            if (arrayToBePartitioned == null)
+                throw new ArgumentNullException(nameof(arrayToBePartitioned));
+            if (k == null)
+                throw new ArgumentNullException(nameof(k));
+            if (arrayToBePartitioned.Length <= 0 || length <= 0)
+                throw new ArgumentOutOfRangeException(nameof(arrayToBePartitioned.Length), "array length or length is invalid");
+            if (k.Length > arrayToBePartitioned.Length)
+                throw new ArgumentException("k array length cannot be greater than the array to be selected length");
+            if (k.Length == 0) return;
+            int shiftRightAmount = sizeof(uint) * 8 - Log2ofPowerOfTwoRadix;
+            // Insertion Sort or Heap Sort could be passed in as another base case since they are both in-place
+            SelectRadixMsdUIntInner(arrayToBePartitioned, start, length, shiftRightAmount, k, 0, k.Length, Array.Sort, threshold);
+            // The following does not work: Need to figure out how to pass InsertionSort method as an Action
+            //RadixSortMsdUIntInner(arrayToBePartitioned, start, length, shiftRightAmount, (arr, startIndex, lengthOfArray) => InsertionSort(arrayToBePartitioned, start, length), threshold);
+        }
+        /// <summary>
+        /// In-place Partition using Radix by the k[]-th elements in an array. Not a stable algorithm.
+        /// </summary>
+        /// <param name="arrayToBePartitioned">array that is to be selected from in place</param>
+        /// <param name="k">array of indexes of the desired elements to be partitioned by, in ascending order</param>
+        /// <param name="threshold">for array size smaller than threshold Array.Sort will be used instead of MSD Radix Sort like algorithm</param>
+        public static void PartitionRadix(this uint[] arrayToBePartitioned, Int32[] k, Int32 threshold = 1024)
+        {
+            if (arrayToBePartitioned == null)
+                throw new ArgumentNullException(nameof(arrayToBePartitioned));
+            if (k == null)
+                throw new ArgumentNullException(nameof(k));
+            if (arrayToBePartitioned.Length <= 0)
+                throw new ArgumentOutOfRangeException(nameof(arrayToBePartitioned.Length), "array length is invalid");
+            if (k.Length > arrayToBePartitioned.Length)
+                throw new ArgumentException("k array length cannot be greater than the array to be selected length");
+            if (k.Length == 0) return;
+            int shiftRightAmount = (sizeof(uint) * 8) - Log2ofPowerOfTwoRadix;
+            SelectRadixMsdUIntInner(arrayToBePartitioned, 0, arrayToBePartitioned.Length, shiftRightAmount, k, 0, k.Length, Array.Sort, threshold);
+        }
+
     }
 }
